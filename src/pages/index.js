3a40b5ece6b45d0d44b3f9560ce6997f3aa8d105
seetam-cto <seetam@creativejoule.in>
2023-04-08 17:20:52 +0530

    fix: images optimized

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
     <>
       <Head>
         <title>Best Digital Marketing Agency In Bengaluru: Okay Done</title>
-        <meta name="description" content="Okay Done is one bangalore&rsquo;s top agencies, that focuses on millennial marketing methods to help brands and organizations reach their optimum potential in the digital space." />
+        <meta name="description" content="Okay Done is one of India's top agencies, that focuses on millennial marketing methods to help brands and organizations reach their optimum potential in the digital space." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js" crossOrigin='anonymous' defer></script>
