d2af0686555aac167c1de8245b044f26f6132206
seetam-cto <seetam@creativejoule.in>
2023-04-12 12:07:51 +0530

    fix: gtag update

--- src/pages/contact-us/index.jsx
+++ src/pages/contact-us/index.jsx
@@ -46,7 +46,8 @@ export default function ContactUs() {
         <meta name="description" content="Okay Done is one bangalore's top agencies, that focuses on millennial marketing methods to help brands and organizations reach their optimum potential in the digital space." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-11060009434" />
+      </Head>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-11060009434" />
         <Script id="google-analytics"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
@@ -54,7 +55,6 @@ export default function ContactUs() {
         gtag('event', 'conversion', {'send_to': 'AW-11060009434/B0CnCO3SypcYENqz6Zkp'});
         `}}
         />
-      </Head>
       <LoaderScreen />
       <main className={"main"}>
         <Background />
