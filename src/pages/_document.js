390f482515ac695c08cd83721cff785d99b054e3
seetam-cto <seetam@creativejoule.in>
2023-04-27 13:17:30 +0530

    feat: pixel code correction

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,22 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="facebook-domain-verification" content="wwkoankymfywphuax29ajrthwbqhp7" />
+        <script type="application/ld+json" strategy={"afterInteractive"}
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '552935527031747');
+            fbq('track', 'PageView');
+          `
+        }}
+      ></script>
       </Head>
       <body>
         <Main />
