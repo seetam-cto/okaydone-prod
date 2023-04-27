3829603adcf4086cbbc32e56459d6ccb81f5d2f6
seetam-cto <seetam@creativejoule.in>
2023-04-27 12:41:04 +0530

    feat: meta verification code added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="facebook-domain-verification" content="wwkoankymfywphuax29ajrthwbqhp7" />
+      </Head>
       <body>
         <Main />
         <NextScript />
