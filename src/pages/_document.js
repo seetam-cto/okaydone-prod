0691e391283bada93a323be59756dfe2be2ea4fe
seetam.divkar <seetam.divkar>
2024-11-26 23:07:22 +0530

    feat: google verification

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="facebook-domain-verification" content="wwkoankymfywphuax29ajrthwbqhp7"/>
+        <meta name="google-site-verification" content="5WD3gHCpBFAxskC0BSj0WES94tVHEcDxwgW-RbIIpC0" />
         <meta name="author" content="Created by Seetam Divkar (seetam.divkar@gmail.com)"/>
       </Head>
       <body>
